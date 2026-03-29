7d7ff04492faaefeb71c5d9f7fb7e9a568d54ea5
wahyudi.indra <wahyudiindra79@gmail.com>
2026-03-30 05:52:00 +0700

    feat: skill test 2

--- app/Http/Controllers/Api/SkillTestController.php
+++ app/Http/Controllers/Api/SkillTestController.php
@@ -36,4 +36,35 @@ class SkillTestController extends Controller
             return $key;
         })->join("");
     }
+
+    public function skillTest2()
+    {
+        // Urutkan array dibawah ini dari yang terkecil sampai terbesar dengan menggunakan perulangan dan kondisi (jangan menggunakan sorting function dari bahasa pemrograman)
+        // Array = [9,3,7,8,2,6,1,4,10,2,2,3]
+
+        // - Define input = [9,3,7,8,2,6,1,4,10,2,2,3]
+        // - Define $len = length of input
+        // - Loop input as $i
+        //      - Loop as $j (0 to $len -$i - 1)
+        //          - Check  arr[j] > arr[j+1]
+        //              - set temp = arr[j]
+        //              - set arr[j] = arr[j+1]
+        //              - set arr[j+1] = temp
+        //      - End loop as $j
+        // - End loop input  as $i
+        // - return input after sorted asc
+
+        $input = [9, 3, 7, 8, 2, 6, 1, 4, 10, 2, 2, 3];
+        $len = count($input);
+        for ($i = 0; $i < $len; $i++) {
+            for ($j = 0; $j < $len - $i - 1; $j++) {
+                if ($input[$j] > $input[$j + 1]) {
+                    $temp = $input[$j];
+                    $input[$j] = $input[$j + 1];
+                    $input[$j + 1] = $temp;
+                }
+            }
+        }
+        return $input;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Api\SkillTestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('skill-test-1', [SkillTestController::class, 'skillTest1']);
+Route::get('skill-test-2', [SkillTestController::class, 'skillTest2']);
